410389a22eac1aec70dfdd14e2c13f6aee2e1ac7
Zermina Ejaz <zerminaejaz@gmail.com>
2020-01-16 18:14:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input){
+    return input.charAt(0).toUpperCase() + input.slice(1)
+
+  }
+
+  static sanitize(input){
+    return input.replace(/[^\w\s]/gi, '')
+  }
+
+  static titleize(input){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    
+    
+    var wordsArray = input.split(" ")
+    wordsArray[0] = Formatter.capitalize(wordsArray[0])
+
+    for(var i = 1; i < wordsArray.length; i++){
+
+    }
+  }
+
 }
