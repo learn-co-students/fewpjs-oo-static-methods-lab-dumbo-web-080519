d85cc1e5f351edad94081a59736f8733e6295354
Zermina Ejaz <zerminaejaz@gmail.com>
2020-01-16 18:21:12 -0500

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ class Formatter {
   }
 
   static sanitize(input){
-    return input.replace(/[^\w\s]/gi, '')
+    return input.replace(/[^A-Za-z0-9 '-]/g, '')
   }
+
 
   static titleize(input){
     let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
@@ -15,10 +16,14 @@ class Formatter {
     
     var wordsArray = input.split(" ")
     wordsArray[0] = Formatter.capitalize(wordsArray[0])
-
-    for(var i = 1; i < wordsArray.length; i++){
-
+    var wordsArrayLength = wordsArray.length
+    for(var i = 1; i < wordsArrayLength; i++){
+        //if that word at the arry exists in the other array, dont capitalize it
+        if(!exceptions.includes(wordsArray[i]))
+          wordsArray[i] = this.capitalize(wordsArray[i])
     }
+
+    return wordsArray.join(" ")
   }
 
 }
